ca203ecda998cacb4e8bb8db4b997ea7670d8736
zkytech <zhangkunyuan@hotmail.com>
2019-12-29 16:14:47 +0800

    优化打包配置

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## 为毕业设计编写的小工具
 
-+ 层次分析（使用均方根计算权重）
++ [层次分析](https://public.zkytech.top/tools/index.html)（使用均方根计算权重）
--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,18 @@ const {
   override,
   fixBabelImports
 } = require('customize-cra')
+const setGlobalObject = value => config => {
+  if (config.mode === "production") {
+    config.output.publicPath = '/tools/'
+  }
 
+  return config
+}
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
-  })
+  }),
+  setGlobalObject()
 )
